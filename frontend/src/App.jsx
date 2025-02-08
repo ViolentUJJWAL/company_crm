07ece9813616b15b895c1f2b2049af4416d7293f
Devanshu atal <146934344+Atal-dev@users.noreply.github.com>
2025-02-08 12:00:21 +0530

    push code for fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,9 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login/Login";
 import ResetPasswordForm from "./Components/Login/ResetPasswordForm";
 import Roles from "./Components/Roles/Roles";
-<<<<<<< HEAD
-import ReminderForm from "./Components/Reminder/ReminderForm";
-import ReminderList from "./Components/Reminder/Reminder";
-=======
 import EmployeeVerification from "./Components/EmployeeVerification/EmployeeVerification";
->>>>>>> origin/nitesh
+import ReminderList from './Components/Reminder/Reminder'
+import ReminderForm from './Components/Reminder/ReminderForm'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -31,13 +28,10 @@ function App() {
           <Route path="/reset-password/:token" element={<ResetPasswordForm/>} />
           <Route path="/lead" element={<Layout><Lead /></Layout>} />
           <Route path="/roles" element={<Layout><Roles /></Layout>} />
-<<<<<<< HEAD
           <Route path="/reminder" element={<Layout><ReminderList /></Layout>} />
           <Route path="/reminderForm" element={<Layout><ReminderForm /></Layout>} />
 
-=======
           <Route path="/employee-verification" element={<Layout><EmployeeVerification /></Layout>} />
->>>>>>> origin/nitesh
         </Routes>
       </BrowserRouter>
     </>
